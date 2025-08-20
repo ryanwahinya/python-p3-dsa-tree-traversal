456ffd82355eb0293ceb2109141b045953b208da
RyanWahinya <ryan.wahinya@student.moringaschool.com>
2025-08-20 17:27:45 +0300

    new

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,14 @@
 class Tree:
   def __init__(self, root = None):
     self.root = root
-
   def get_element_by_id(self, id):
+    self.stack = []
+    self.stack.append(self.root)
+    while self.stack:
+      node = self.stack.pop()
+      if node['id'] == id:
+        return node
+      else:
+        for child in node['children']:
+          self.stack.append(child)
     pass
